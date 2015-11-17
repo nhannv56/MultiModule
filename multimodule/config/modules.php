cf84f10f24cb8cdcd5699bd4aed0864d506842a6
Nhan <ngo.nhan.09@gmail.com>
2015-11-17 21:20:07 +0700

    [multimodule]add module config

--- multimodule/config/modules.php
+++ multimodule/config/modules.php
@@ -3,9 +3,14 @@
 /**
  * Register application modules
  */
-$application->registerModules(array(
-    'frontend' => array(
-        'className' => 'multimodule\Frontend\Module',
-        'path' => __DIR__ . '/../apps/frontend/Module.php'
-    )
-));
+$application->registerModules ( array (
+		'frontend' => array (
+				'className' => 'multimodule\Frontend\Module',
+				'path' => __DIR__ . '/../apps/frontend/Module.php' 
+		),
+		'backend' => array (
+				'className' => 'multimodule\Backend\Module',
+				'path' => __DIR__ . '/../apps/backend/Module.php' 
+		) 
+)
+ );
