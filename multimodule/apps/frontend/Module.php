6d5040b907ffbc9e0954c701b9a004e21df05c78
Nhan <ngo.nhan.09@gmail.com>
2015-11-17 20:52:04 +0700

    [multimodule] re-setup multi module

---
+++ multimodule/apps/frontend/Module.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace multimodule\Frontend;
+
+use Phalcon\DiInterface;
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Mvc\ModuleDefinitionInterface;
+use Phalcon\Config\Adapter\Ini;
+
+class Module implements ModuleDefinitionInterface
+{
+    /**
+     * Registers an autoloader related to the module
+     *
+     * @param DiInterface $di
+     */
+    public function registerAutoloaders(DiInterface $di = null)
+    {
+
+        $loader = new Loader();
+
+        $loader->registerNamespaces(array(
+            'multimodule\Frontend\Controllers' => __DIR__ . '/controllers/',
+            'multimodule\Frontend\Models' => __DIR__ . '/models/',
+        ));
+
+        $loader->register();
+    }
+
+    /**
+     * Registers services related to the module
+     *
+     * @param DiInterface $di
+     */
+    public function registerServices(DiInterface $di)
+    {
+        /**
+         * Read configuration
+         */
+        $config = new Ini(APP_PATH  . "/apps/frontend/config/config.ini");
+
+        /**
+         * Setting up the view component
+         */
+        $di['view'] = function () {
+            $view = new View();
+            $view->setViewsDir(__DIR__ . '/views/');
+
+            return $view;
+        };
+
+        /**
+         * Database connection is created based in the parameters defined in the configuration file
+         */
+        $di['db'] = function () use ($config) {
+            return new DbAdapter($config->toArray());
+        };
+    }
+}
---
+++ multimodule/apps/frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Multimodule\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
