f3eb01f5d1dcb4cd85d8b861fa35bcb884cfafe8
Nhan <ngo.nhan.09@gmail.com>
2015-11-17 21:17:10 +0700

    [[multimodule]add backend module

---
+++ multimodule/apps/backend/controllers/ControllerBase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace multimodule\Backend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+}
---
+++ multimodule/apps/backend/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace multimodule\Backend\Controllers;
+
+use multimodule\Backend\Controllers\ControllerBase;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
